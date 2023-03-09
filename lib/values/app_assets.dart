d62f044f610a24cbb39eb560bb2c8b935412281d
Thangnb <nbthang2212@gmail.com>
2023-03-09 16:06:39 +0700

    thêm đường dẫn với các ảnh

--- lib/values/app_assets.dart
+++ lib/values/app_assets.dart
@@ -1,23 +1,46 @@
 import 'package:flutter/animation.dart';
 
 class AppColors {
+// appearance
   static const Color backgroundColor = Color(0xffc280ff);
   static const Color appBar = Color(0xff6a1999);
 
+// text
   static const Color placehoder = Color(0xfff2e4f5);
   static const Color plaintext = Color(0xfff2e4f5);
   static const Color title = Color(0xff49148a);
-
-  static const Color number = Color(0xfff59e2b);
   static const Color note = Color(0xffe3e3e3);
+  static const Color number = Color(0xfff59e2b);
 
+// button
   static const Color buttonNumber = Color(0xff9b26b2);
   static const Color buttonAction = Color(0xff6a1999);
   static const Color buttonAction2 = Color(0xffddb8ff);
   static const Color buttonAction3 = Color(0xffe0bde7);
 
+// view
   static const Color status = Color(0xff7920a2);
   static const Color status2 = Color(0xffac01ff);
   static const Color statusLight = Color(0xffac01ff);
   static const Color statusLight2 = Color(0xfff2e4f5);
+}
+
+class AppAssets {
+  static String imgPath = 'assets/images/';
+
+  static final String iBackward = '${imgPath}back.png';
+  static final String iBlueTick = '${imgPath}blue_tick.png';
+  static final String iCalendar = '${imgPath}calendar.png';
+  static final String iFootPrint = '${imgPath}footprint.png';
+  static final String iLeaf = '${imgPath}leaf.png';
+  static final String iFootprint = '${imgPath}footprint.png';
+  static final String iLeftLeaf = '${imgPath}left_leaf.png';
+  static final String iMinus = '${imgPath}minus.png';
+  static final String iMoney = '${imgPath}money.png';
+  static final String iPencil = '${imgPath}pencil.png';
+  static final String iPerson = '${imgPath}person.png';
+  static final String iPlus = '${imgPath}plus.png';
+  static final String iRedX = '${imgPath}red_x.png';
+  static final String iSave = '${imgPath}save.png';
+  static final String iSaveMoney = '${imgPath}savemoney.png';
 }

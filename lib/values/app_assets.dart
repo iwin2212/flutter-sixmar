411906e00d7cda6a55fb262059d99bc787a73972
Thangnb <nbthang2212@gmail.com>
2023-03-09 15:50:53 +0700

    Thêm mã màu mới

---
+++ lib/values/app_assets.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/animation.dart';
+
+class AppColors {
+  static const Color backgroundColor = Color(0xffc280ff);
+  static const Color appBar = Color(0xff6a1999);
+
+  static const Color placehoder = Color(0xfff2e4f5);
+  static const Color plaintext = Color(0xfff2e4f5);
+  static const Color title = Color(0xff49148a);
+
+  static const Color number = Color(0xfff59e2b);
+  static const Color note = Color(0xffe3e3e3);
+
+  static const Color buttonNumber = Color(0xff9b26b2);
+  static const Color buttonAction = Color(0xff6a1999);
+  static const Color buttonAction2 = Color(0xffddb8ff);
+  static const Color buttonAction3 = Color(0xffe0bde7);
+
+  static const Color status = Color(0xff7920a2);
+  static const Color status2 = Color(0xffac01ff);
+  static const Color statusLight = Color(0xffac01ff);
+  static const Color statusLight2 = Color(0xfff2e4f5);
+}
